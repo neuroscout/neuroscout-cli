ac55a8d315bbf6e6c04fa772b8575b100e83101a
Alejandro de la Vega <aleph4@gmail.com>
2022-02-07 17:28:23 -0600

    Add comments

---
+++ neuroscout_cli/tools/convert.py
@@ -0,0 +1,51 @@
+import json
+from copy import deepcopy
+
+def convert_to_v1(model):
+    """ Convert a 0.x BIDS Stats Model to v1.
+    Currently tailed to convert simple Neuroscout-style models """
+    model = deepcopy(model)
+    
+    # Add version
+    model["BIDSModelVersion"] = 1.0
+    
+    # Steps -> Nodes
+    model["Nodes"] = model.pop("Steps")
+    
+    for node in model["Nodes"]:
+        # Level == Name
+        node["Name"] = node["Level"]
+        
+        # Model is mandatory, add constant
+        if "Model" not in node:
+            node['Model'] = {}
+            node['Model']['X'] = [1]
+        else:
+            node['Model']['X'].append(1)
+             
+        # Contrast "type" -> "Test". Add default. 
+        if "Contrasts" in node:
+            for con in node["Contrasts"]:
+                if "type" in con:
+                    con["Test"] = con.pop("type")
+                else:
+                    con["Test"] = "t"
+                
+        # DummyContrast "Type" -> "Test"
+        # add "Type" = "Meta" to "X"
+        if "DummyContrasts" in node:
+            node['DummyContrasts']['Test'] = \
+                node['DummyContrasts'].pop('Type')
+            
+            if node['DummyContrasts']['Test'] == 'FEMA':
+                node['DummyContrasts']['Test'] = 't'
+                node['Model']['Type'] = 'Meta'
+                
+        # Indirect Transformations call
+        if "Transformations" in node:
+            node["Transformations"] = {
+                "Transformer": "pybids-transforms-v1",
+                "Instructions": node.pop("Transformations")
+            }
+    
+    return model
