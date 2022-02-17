f68575243e293d4677ae639716d36e8679a6efa5
Alejandro de la Vega <aleph4@gmail.com>
2022-02-16 19:41:53 -0600

    optional

--- neuroscout_cli/cli.py
+++ neuroscout_cli/cli.py
@@ -2,7 +2,7 @@
 neuroscout-cli
 
 Usage:
-    neuroscout run [options] <outdir> <bundle_id> (<fitlins_args>...)
+    neuroscout run [options] <outdir> <bundle_id> [<fitlins_args>...]
     neuroscout install [--install-dir <dir>] <outdir> <bundle_id>
     neuroscout upload [--force-upload --neurovault <nv>] <outdir> <bundle_id>
     neuroscout -h | --help
