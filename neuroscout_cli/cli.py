21ec4aabe122ed3cd9b0b2b4b7804c9f6c93fb46
Alejandro de la Vega <aleph4@gmail.com>
2022-03-29 17:01:01 -0500

    Set datalad argument type

--- neuroscout_cli/cli.py
+++ neuroscout_cli/cli.py
@@ -42,7 +42,7 @@ def main():
 @click.option('--upload-first-level', is_flag=True, help='Upload first-level results, in addition to group')
 @click.option('--force-upload', is_flag=True, help='Force upload even if a NV collection already exists')
 @click.option('--no-get', is_flag=True, help="Don't automatically fetch bundle & dataset")
-@click.option('--datalad-jobs', help='Number of parallel jobs for DataLad when fetching files', default='auto')
+@click.option('--datalad-jobs', help='Number of parallel jobs for DataLad when fetching files', default=1, type=click.INT)
 @click.option('--download-dir', help='Directory to cache input datasets, instead of OUT_DIR', type=click.Path())
 @click.argument('fitlins_options', nargs=-1, type=click.UNPROCESSED)
 @main.command(context_settings=dict(
@@ -77,7 +77,7 @@ def run(**kwargs):
 @click.argument('analysis_id')
 @click.option('--bundle-only', is_flag=True, help="Only fetch analysis bundle, not imaging data")
 @click.option('--download-dir', help='Directory to cache input datasets, instead of OUT_DIR', type=click.Path())
-@click.option('--datalad-jobs', help='Number of parallel jobs for DataLad when fetching files', default='auto')
+@click.option('--datalad-jobs', help='Number of parallel jobs for DataLad when fetching files', default=1, type=click.INT)
 @main.command()
 def get(**kwargs):
     """ Fetch analysis inputs.
